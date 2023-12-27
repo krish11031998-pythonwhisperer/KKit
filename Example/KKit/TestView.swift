b6abd3044c61badbb9632541ef48d35714fc41b5
Krishna Venkatramani <krishna@getbaraka.com>
2023-12-27 13:03:28 +0000

    Initial commit

---
+++ Example/KKit/TestView.swift
@@ -0,0 +1,79 @@
+//
+//  TestView.swift
+//  SUIKit_Example
+//
+//  Created by Krishna Venkatramani on 27/12/2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import KKit
+
+class TestView: UIView {
+    
+    private var largeViewWidthConstraint: NSLayoutConstraint!
+    private var secondLargeViewHeightConstraint: NSLayoutConstraint!
+    
+    private lazy var largeSimpleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    private lazy var secondLargeSimpleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    private lazy var smallSimpleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        largeViewWidthConstraint.constant = bounds.width.half
+        secondLargeViewHeightConstraint.constant = bounds.height * 0.6
+    }
+    
+    private func setupView() {
+        [largeSimpleView, secondLargeSimpleView, smallSimpleView].forEach(addSubview(_:))
+        
+        largeSimpleView
+            .pinTopAnchorTo(constant: 0)
+            .pinLeadingAnchorTo(constant: 0)
+            .pinBottomAnchorTo(constant: 0)
+        
+        secondLargeSimpleView
+            .pinTopAnchorTo(constant: 0)
+            .pinLeadingAnchorTo(largeSimpleView, anchor: \.trailingAnchor, constant: 0)
+            .pinTrailingAnchorTo(constant: 0)
+        
+        smallSimpleView
+            .pinTopAnchorTo(secondLargeSimpleView, anchor: \.bottomAnchor, constant: 0)
+            .pinLeadingAnchorTo(largeSimpleView, anchor: \.trailingAnchor, constant: 0)
+            .pinTrailingAnchorTo(constant: 0)
+            .pinBottomAnchorTo(constant: 0)
+        
+        largeViewWidthConstraint = largeSimpleView.setWidth(width: 100)
+        secondLargeViewHeightConstraint = secondLargeSimpleView.setHeight(height: 100)
+            
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview("Test", traits: .fixedLayout(width: 300, height: 500), body: {
+    let view = TestView()
+    return view
+})
---
+++ Example/Pods/Target Support Files/KKit/KKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KKit : NSObject
+@end
+@implementation PodsDummy_KKit
+@end
