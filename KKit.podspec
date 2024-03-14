216427f7ed0635efc758b95c485b1276c410025c
Krishna <krish_venkat11@hotmail.com>
2024-03-14 20:46:09 +0100

    Change limit

--- KKit.podspec
+++ KKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'KKit'
-  s.version          = '1.0.7'
+  s.version          = '1.0.8'
   s.summary          = 'Simple to use UIKit Helper'
 
 # This description is used to generate tags and improve search results.
--- KKit/Classes/Misc/ArrayExtension.swift
+++ KKit/Classes/Misc/ArrayExtension.swift
@@ -10,6 +10,6 @@ import Foundation
 public extension Array {
     func limit(from: Int = 0, to: Int) -> [Self.Element] {
         guard from < count - 1 else { return self }
-        return to <= count - 1 ? Array(self[from...to]) : self
+        return to <= count - 1 ? Array(self[from..<to]) : self
     }
 }
